ea447d407dd8ee859c92d27b7f45e2f1ea978774
Kevin A. Hoogheem <kevin@hoogheem.net>
2019-04-13 15:04:19 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FitnessUnits",
+    // platforms: [.iOS("10.0"), .macOS("10.12"), .tvOS("10.0"), .watchOS("3.0")],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
